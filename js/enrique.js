65cdf6d7b6124d2c9844be234f86978d1123c275
Enrique <egonzalez78@gmail.com>
2015-06-01 11:38:28 -0700

    switched from ngRoute to ui-router

--- js/enrique.js
+++ js/enrique.js
@@ -1,31 +1,35 @@
 (function(){
-var app = angular.module('enrique', ['ngAnimate', 'ngRoute']);
+var app = angular.module('enrique', ['ngAnimate', 'ui.router']);
 
-app.config(['$routeProvider', '$locationProvider',
-  function($routeProvider, $locationProvider) {
-    $routeProvider
-      .when('/', {
+app.config(['$urlRouterProvider', '$stateProvider',
+  function($urlRouterProvider, $stateProvider) {
+
+    $urlRouterProvider
+    .otherwise('/');
+
+    $stateProvider
+      .state('home', {
+        url: '/',
         templateUrl: 'templates/work/work.html'
       })
-      .when('/work/gravitytank-feedback', {
+      .state('prototyping', {
+        url: '/work/gravitytank-feedback',
         templateUrl: 'templates/work/gravitytank-feedback.html'
       })
-      .when('/work/digital-storytelling', {
+      .state('storytelling', {
+        url: '/work/digital-storytelling',
         templateUrl: 'templates/work/digital-storytelling.html'
       })
-      .when('/work/simple-impactful-innovation', {
+      .state('innovation', {
+        url: '/work/simple-impactful-innovation',
         templateUrl: 'templates/work/simple-impactful-innovation.html'
-      })
-      .otherwise({
-        redirectTo: '/'
       });
 
-    $locationProvider.html5Mode(true);
-}])
+}]);
 
 app.controller('ContactCtrl', function($scope){
   $scope.email = 'egonzalez78@gmail.com'
-})
+});
 
 
 app.controller('TabCtrl', function($scope, $location){
